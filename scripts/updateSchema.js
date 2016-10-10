74971b0fb35c7be8810639bbdaed056868f6cdc1
18601673727 <18601673727@163.com>
2016-10-11 03:57:26 +0800

    first commit

--- scripts/updateSchema.js
+++ scripts/updateSchema.js
@@ -1,29 +1,33 @@
-#!/usr/bin/env babel-node --optional es7.asyncFunctions
-
-import fs from 'fs';
-import path from 'path';
-import { Schema } from '../data/schema';
-import { graphql }  from 'graphql';
-import { introspectionQuery, printSchema } from 'graphql/utilities';
+const fetch = require('node-fetch');
+const fs = require('fs');
+const {
+  buildClientSchema,
+  introspectionQuery,
+  printSchema,
+} = require('graphql/utilities');
+const path = require('path');
+const schemaPath = path.join(__dirname, '../data/schema');
+const ENDPOINT = 'http://graphql-swapi.parseapp.com/';
 
 // Save JSON of full schema introspection for Babel Relay Plugin to use
-(async () => {
-  var result = await (graphql(Schema, introspectionQuery));
-  if (result.errors) {
-    console.error(
-      'ERROR introspecting schema: ',
-      JSON.stringify(result.errors, null, 2)
-    );
-  } else {
-    fs.writeFileSync(
-      path.join(__dirname, '../data/schema.json'),
-      JSON.stringify(result, null, 2)
-    );
-  }
-})();
+fetch(`${ENDPOINT}`, {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({'query': introspectionQuery}),
+}).then(res => res.json()).then(schemaJSON => {
+  fs.writeFileSync(
+    `${schemaPath}.json`,
+    JSON.stringify(schemaJSON, null, 2)
+  );
 
-// Save user readable type system shorthand of schema
-fs.writeFileSync(
-  path.join(__dirname, '../data/schema.graphql'),
-  printSchema(Schema)
-);
+  // Save user readable type system shorthand of schema
+  const graphQLSchema = buildClientSchema(schemaJSON.data);
+
+  fs.writeFileSync(
+    `${schemaPath}.graphql`,
+    printSchema(graphQLSchema)
+  );
+});
